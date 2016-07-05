044a0ba93d329329285f0e5c6e64a710b22bfc1c
Tatsuya Ogi <t.oginogin@gmail.com>
2016-07-05 20:41:41 +0900

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -20,9 +19,8 @@ ActiveRecord::Schema.define(version: 20160126015213) do
     t.datetime "updated_at",       null: false
     t.integer  "user_id"
     t.datetime "last_modified_at"
+    t.index ["user_id"], name: "index_fasta_data_on_user_id"
   end
-
-  add_index "fasta_data", ["user_id"], name: "index_fasta_data_on_user_id"
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -38,9 +36,8 @@ ActiveRecord::Schema.define(version: 20160126015213) do
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
     t.string   "name"
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
-
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
 
 end
